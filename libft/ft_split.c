c7da44574fce43fe142daa41bd7df6323a327828
Julie Blaye <jblaye@paul-f4Br7s5.clusters.42paris.fr>
2023-12-20 15:59:06 +0100

    preliminary work somehow

---
+++ libft/ft_split.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 17:26:50 by jblaye            #+#    #+#             */
+/*   Updated: 2023/11/10 20:14:06 by jblaye           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	wordlen(char const *s, char c)
+{
+	size_t	wlen;
+
+	wlen = 0;
+	while (*s != c && *s != 0)
+	{
+		s++;
+		wlen++;
+	}
+	return (wlen);
+}
+
+size_t	wordcount(char const *s, char c)
+{
+	size_t	count;
+
+	count = 0;
+	if (!s)
+		return (0);
+	while (*s != 0)
+	{
+		while (*s == c)
+			s++;
+		if (*s != 0)
+			count++;
+		while (*s != c && *s != 0)
+			s++;
+	}
+	return (count);
+}
+
+char	*partialstrdup(char const *s, char c)
+{
+	size_t	wlen;
+	size_t	i;
+	char	*r;
+
+	wlen = wordlen(s, c);
+	i = 0;
+	r = (char *) malloc(sizeof(char) * (wlen + 1));
+	if (!r)
+		return (0);
+	while (i < wlen)
+	{
+		r[i] = *s;
+		s++;
+		i++;
+	}
+	r[i] = 0;
+	return (r);
+}
+
+char	**mallocprotec(char **r, int len)
+{
+	int	i;
+	int	success;
+
+	i = 0;
+	success = 1;
+	while (i < len)
+	{
+		if (!r[i])
+			success = 0;
+		i++;
+	}
+	if (success == 0)
+	{
+		i = 0;
+		while (i <= len)
+		{
+			if (r[i])
+				free(r[i]);
+			i++;
+		}
+		free(r);
+		return (0);
+	}
+	return (r);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**r;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	r = (char **) ft_calloc(wordcount(s, c) + 1, sizeof(char *));
+	if (!r)
+		return (0);
+	while (*s != 0)
+	{
+		while (*s != 0 && *s == c)
+			s++;
+		if (*s != 0)
+		{
+			r[i] = partialstrdup(s, c);
+			i++;
+		}
+		while (*s != c && *s != 0)
+			s++;
+	}
+	r[i] = NULL;
+	r = mallocprotec(r, i);
+	return (r);
+}
--- pipex.h
+++ pipex.h
@@ -1,38 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/19 13:18:14 by jblaye            #+#    #+#             */
-/*   Updated: 2023/12/20 15:58:51 by jblaye           ###   ########.fr       */
+/*   Created: 2023/12/20 14:16:01 by jblaye            #+#    #+#             */
+/*   Updated: 2023/12/20 14:32:05 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#ifndef PIPEX_H
+# define PIPEX_H
 
-int	main(int ac, char **av)
-{
-	char	**exav;
-	char	**exenvp;
-	__pid_t	pid;
-	int		pipefd[2];
-	int		i;
-	
-	i = 1;
-	while (i < ac - 1)
-	{
-		exenvp[0] = NULL;
-		exav = ft_split(av[2], ' ');
-		if (pipe(pipefd) == -1)
-			return (perror("pipe"), 1);
-		pid = fork();
-		if (pid == -1)
-			return (perror("fork"), 1);
-		else if (pid == 0)
-		{
-			
-		}
-	}
-}
+# include <unistd.h>
+# include <stdio.h>
+
+char	**ft_split(char const *s, char c);
+
+#endif

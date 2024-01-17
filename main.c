61fe469e44f689b78bd708690f0c7bc513f079cb
Julie Blaye <jblaye@bess-f1r1s5.clusters.42paris.fr>
2024-01-17 17:28:53 +0100

    end of day commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 13:18:14 by jblaye            #+#    #+#             */
-/*   Updated: 2024/01/17 16:53:07 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/01/17 17:14:27 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,21 +41,21 @@ int	exec_child(char *full_cmd, int fds[4], int fdio[2], char **ev)
 	char	**execav;
 	char	*path;
 	pid_t	pid;
-	
+
 	pid = fork();
 	if (pid == -1)
 		return (perror("fork"), -1);
 	if (pid == 0)
 	{
 		if (dup2(fds[0], 0) == -1 || dup2(fds[3], 1) == -1)
-			(perror("dup"), ft_dprintf(2, "error\n"), exit(-1));
+			perror("dup");
 		close_fd(fds, fdio);
 		execav = ft_split(full_cmd, ' ');
 		if (!execav)
 			return (ft_dprintf(2, "Memory error"), -1);
 		path = find_path(execav[0], ev);
 		if (!path)
-			(ft_freesplit(execav), ft_dprintf(2, "error\n"), exit(-1));
+			(ft_freesplit(execav), exit(-1));
 		if (path != 0 && execve(path, execav, ev) < 0)
 			(perror("execve"), free(execav), free(path), exit(-1));
 	}
@@ -72,7 +72,6 @@ void	exec_multipipe(int cmd_nbr, char **cmds, char **ev, int fdio[2])
 	int	pid;
 
 	i = 0;
-	ft_dprintf(2, "test2:%s\n", cmds[i]);
 	if (pipe(fdpipe) == -1)
 		return (perror("pipe"));
 	fds = (int []){fdio[0], -1, fdpipe[0], fdpipe[1]};
@@ -111,6 +110,7 @@ int	main(int ac, char **av, char **ev)
 	int	fdio[2];
 	int	cmd_nbr;
 
+	cmd_nbr = 0;
 	process_fdio(&fdio[0], &fdio[1], ac, av);
 	if ((ft_strncmp("here_doc", av[1], 9) == 0 && ac < 6) || ac < 5)
 		return (ft_dprintf(2, "Invalid arguments"));
@@ -124,7 +124,6 @@ int	main(int ac, char **av, char **ev)
 	else
 	{
 		cmd_nbr = ac - 3;
-		ft_dprintf(2, "test1:%s\n", av[2]);
 		exec_multipipe(cmd_nbr, &av[2], ev, fdio);
 	}
 	return (0);
